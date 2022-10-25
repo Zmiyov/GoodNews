7223380c8e5d69b1b589d2c16e4eeac121e6f6ec
Volodymyr <zmiyov88@gmail.com>
2022-10-25 23:43:50 +0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Vladimir Pisarenko on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
